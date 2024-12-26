c6bfeabeb92d5f9311867289c5cdba1f4d78806a
matteoQuerini <matteoqxx@gmail.com>
2024-12-26 14:44:25 +0100

    Initial commit

---
+++ codice/Nave.java
@@ -0,0 +1,31 @@
+class Nave{
+    //Attributi:
+
+    //Nome della nave
+    protected String nome;
+    //Lunghezza della nave
+    protected int lunghezza;
+    //Stato dei segmenti della nave, se è stata colpita o meno
+    protected boolean[] stato;
+    //Indica se la nave è stata affondata
+    protected boolean affondata;
+    //Indica le coordinate della nave
+    protected int x;
+    protected int y;
+    //Indica l'orientamento della nave
+    protected PuntiCardinali orientamento;
+
+
+    //Costruttore:
+    public Nave(String nome, int lunghezza, int x, int y, PuntiCardinali orientamento){
+        this.nome = nome;
+        this.lunghezza = lunghezza;
+        this.stato = new boolean[lunghezza];
+        this.affondata = false;
+        this.x = x;
+        this.y = y;
+        this.orientamento = orientamento;
+
+        
+    }
+}

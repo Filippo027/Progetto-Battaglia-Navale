9b87ac887d2c1b087353a209c685d2a93bdc3e2d
matteoQuerini <matteoqxx@gmail.com>
2024-12-26 14:04:01 +0100

    Modifica attributo e aggiunta del costruttore

--- codice/Nave.java
+++ codice/Nave.java
@@ -5,14 +5,22 @@ class Nave{
     protected String nome;
     //Lunghezza della nave
     protected int lunghezza;
-    //Stato dei segmenti della nave
+    //Stato dei segmenti della nave, se è stata colpita o meno
     protected boolean[] stato;
-    //Indica se la nave è affondata
+    //Indica se la nave è stata affondata
     protected boolean affondata;
-    //Indica la posizione della nave
-    protected int[][] posizione;
-    
+    //Indica le coordinate della nave
+    protected int x;
+    protected int y;
 
-    
 
+    //Costruttore:
+    public Nave(String nome, int lunghezza, int x, int y){
+        this.nome = nome;
+        this.lunghezza = lunghezza;
+        this.stato = new boolean[lunghezza];
+        this.affondata = false;
+        this.x = x;
+        this.y = y;
+    }
 }

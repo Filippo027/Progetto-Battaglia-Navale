5e43f32ef035f1a5fb4425483088c8d9bb8d8f18
matteoQuerini <matteoqxx@gmail.com>
2024-12-26 12:11:21 +0100

    Creazione classe nave

---
+++ codice/Nave.java
@@ -0,0 +1,18 @@
+class Nave{
+    //Attributi:
+
+    //Nome della nave
+    protected String nome;
+    //Lunghezza della nave
+    protected int lunghezza;
+    //Stato dei segmenti della nave
+    protected boolean[] stato;
+    //Indica se la nave è affondata
+    protected boolean affondata;
+    //Indica la posizione della nave
+    protected int[][] posizione;
+    
+
+    
+
+}

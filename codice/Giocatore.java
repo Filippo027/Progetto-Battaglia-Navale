8cec90c98863ffd7b8fd5913c31e7cdc147fbfda
matteoQuerini <matteoqxx@gmail.com>
2024-12-27 10:52:39 +0100

    Implementazione costruttore nella classe Giocatore.

--- codice/Giocatore.java
+++ codice/Giocatore.java
@@ -2,9 +2,16 @@ import java.util.ArrayList
 
 
 class Giocatore{
-    //Attrubuti:
+    //Attrubuti
     private String nome;
     private Griglia griglia;
-    private ArrayList<Nave> navi = new ArrayList<Nave>();
+    private ArrayList<Nave> navi;
+
+    //Costruttore
+    public Giocatore(String nome, Griglia griglia){
+        this.nome = nome;
+        this.griglia = griglia;
+        this.navi = new ArrayList<Nave>();
+    }
     
 }

ea262e272ca720ded442e6620dd824dca5ae6918
matteoQuerini <matteoqxx@gmail.com>
2024-12-27 10:50:36 +0100

    Creazione classe giocatore.

---
+++ codice/Giocatore.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList
+
+
+class Giocatore{
+    //Attrubuti:
+    private String nome;
+    private Griglia griglia;
+    private ArrayList<Nave> navi = new ArrayList<Nave>();
+    
+}

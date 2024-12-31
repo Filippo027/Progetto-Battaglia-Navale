f8f1736ce7e6428f99eef5900de8877a3c72432d
matteoQuerini <matteoqxx@gmail.com>
2024-12-31 12:04:04 +0100

    Modifica.

--- codice/Giocatore.java
+++ codice/Giocatore.java
@@ -4,8 +4,8 @@ import java.util.ArrayList
 class Giocatore{
     //Attrubuti
     private String nome;
-    private Griglia griglia;
     private ArrayList<Nave> navi;
+    
 
     //Costruttore
     public Giocatore(String nome, Griglia griglia){

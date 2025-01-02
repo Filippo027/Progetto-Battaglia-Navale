bf48e94296b8341d419751049bba399263cfb1b5
Gabrielit018 <gabriele.zampis@allievi.bearzi.it>
2025-01-02 19:52:33 +0100

    Add files via upload

--- codice/Cella.java
+++ codice/Cella.java
@@ -1,20 +1,20 @@
 public class Cella {
     private Boolean assegnato;
     private Boolean colpito;
-    private int rows=3;
-    private int columns=5;
-    char[][] cella = new char[rows][columns];
+    private int righe=3;
+    private int colonne=5;
+    char[][] cella = new char[righe][colonne];
     public Cella(){
         assegnato=false;
         colpito=false;
         inizializzaCella();
     }
     private void inizializzaCella(){
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<columns; j++){
-                if(j==0 || j==columns-1){
+        for(int i=0; i<righe; i++){
+            for(int j=0; j<colonne; j++){
+                if(j==0 || j==colonne-1){
                     cella[i][j]='|';
-                }else if((i==0 || i==rows-1) && (j>0 && j<columns-1)){
+                }else if((i==0 || i==righe-1) && (j>0 && j<colonne-1)){
                     cella[i][j]='-';
                 }else{
                     cella[i][j]=' ';
@@ -22,12 +22,14 @@ public class Cella {
             }
         }
     }
-    private void printCella(){
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<columns; j++){
+    public void printCella(){
+        for(int i=0; i<righe; i++){
+            for(int j=0; j<colonne; j++){
                 System.out.print(cella[i][j]);
             }
-            System.out.println();
+            if(i<righe-1){
+                System.out.println();
+            }
         }
     }
     public Boolean getAssegnato(){
@@ -42,10 +44,7 @@ public class Cella {
     public void setAssegnato(){
           assegnato=true;
     }
-    public static void main(String[] args) {
-        Cella cella= new Cella();
-        cella.printCella();
-    }
+    
 
     
 }
--- codice/Griglia.java
+++ codice/Griglia.java
@@ -1,16 +1,31 @@
 public class Griglia {
     private int righe=9;
     private int colonne=7;
-    
-    Cella [][] griglia;
+
+    Cella [][] griglia= new Cella[righe][colonne];
     public Griglia(){
         inizializzaGriglia();
     }
-    void inizializzaGriglia(){
+    private void inizializzaGriglia(){
         for(int i=0; i<righe; i++){
             for(int j=0; j<colonne; j++){
                 griglia[i][j]=new Cella();
             }
         }
+    }
+    public void printGriglia(){
+        for(int i=0; i<righe; i++){
+            for(int j=0; j<colonne; j++){
+                //if(j==0 || i==0){
+                    //System.out.print("hdhd");
+                //}else{
+                    griglia[i][j].printCella();
+                //}
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Griglia griglia=new Griglia();
+        griglia.printGriglia();
     }
 }

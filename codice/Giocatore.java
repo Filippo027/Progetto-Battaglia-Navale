0ac468fd2ba8ec59a23cfd3e148edb5e4fd0c6ee
matteoQuerini <matteoqxx@gmail.com>
2025-01-02 09:14:51 +0100

    Aggiunta metodo per aggiungere una nave.

--- codice/Giocatore.java
+++ codice/Giocatore.java
@@ -5,6 +5,7 @@ class Giocatore{
     //Attrubuti
     private String nome;
     private ArrayList<Nave> navi;
+    private Griglia griglia;
     
 
     //Costruttore
@@ -12,6 +13,11 @@ class Giocatore{
         this.nome = nome;
         this.griglia = new Griglia();
         this.navi = new ArrayList<Nave>();
+    }
+
+    protected void aggiungiNave(String nome, int lunghezza, int x, int y, PuntiCardinali orientamento){
+        Nave nuovaNave = new Nave(nome, lunghezza, x, y, orientamento);
+        this.navi.add(nuovaNave);
     }
     
     

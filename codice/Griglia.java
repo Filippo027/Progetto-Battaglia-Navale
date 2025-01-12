e3d35fb334cc72dbbda4a5e0df5050ba4c9f90ca
Gabrielit018 <gabriele.zampis@allievi.bearzi.it>
2025-01-12 17:33:48 +0100

    Add files via upload

--- codice/Griglia.java
+++ codice/Griglia.java
@@ -49,6 +49,7 @@ public class Griglia {
         }
         if(!griglia[rigaInizio][colonnaInizio].getColpito()){
             griglia[rigaInizio][colonnaInizio].setColpito();
+            System.out.println("hai colpito una parte della nave!");
         }else{
             System.out.println("cella già colpita precedentemente, reinserisci le coordinate");
             return false;
@@ -71,6 +72,7 @@ public class Griglia {
             return 3;
         }else if(numeroCelleNaviAssegnabili==5 || numeroCelleNaviAssegnabili==3){
             numeroCelleNaviAssegnabili=numeroCelleNaviAssegnabili-2;
+            return 2;
         }else if(numeroCelleNaviAssegnabili==1){
             numeroCelleNaviAssegnabili--;
             return 1;
@@ -85,14 +87,22 @@ public class Griglia {
         }while(numeroCelleNaviAssegnabili!=0);
 
     }
-    private boolean checkPosizione(int rigaInizio, int colonnaInizio, int rigaFine, int colonnaFine){
+
+    //controlli per l'inserimento quindi la configurazione della nave
+    private boolean checkPosizione(int rigaInizio, int colonnaInizio, int rigaFine, int colonnaFine, int naveDaAssegnare){
         if((rigaInizio != rigaFine) && (colonnaInizio != colonnaFine) ){
             System.out.println("le due coordinate non hanno ne la stessa riga, ne la stessa colonna, reinserisci");
             return false;
         }
-        if((rigaInizio == rigaFine) && (colonnaInizio == colonnaFine)){
-            System.out.println("hai inserito due coordinate uguali, reinserisci");
-            return false;
+        if(naveDaAssegnare!=1){
+            if((rigaInizio == rigaFine) && (colonnaInizio == colonnaFine)){
+                System.out.println("hai inserito due coordinate uguali, reinserisci");
+                return false;
+            }
+        }else if(naveDaAssegnare==1){
+            if((rigaInizio == rigaFine) && (colonnaInizio == colonnaFine)){
+                return true;
+            }
         }
         if((rigaInizio==rigaFine) && (colonnaInizio!=colonnaFine)){
             return true;
@@ -104,19 +114,26 @@ public class Griglia {
     }
     private boolean checkDistanza(int rigaInizio,int colonnaInizio,int rigaFine,int colonnaFine, int naveDaAssegnare){
         int distanza=0;
-        if(rigaInizio==rigaFine){
-            if(colonnaInizio<colonnaFine){
-                distanza=colonnaFine-colonnaInizio+1;
-            }else{
-                distanza=colonnaInizio-colonnaFine+1;
+        if(naveDaAssegnare!=1){
+            if(rigaInizio==rigaFine){
+                if(colonnaInizio<colonnaFine){
+                    distanza=colonnaFine-colonnaInizio+1;
+                }else{
+                    distanza=colonnaInizio-colonnaFine+1;
+                }
+            }else if(colonnaInizio==colonnaFine){
+                if(rigaInizio<rigaFine){
+                    distanza=rigaFine-rigaInizio+1;
+                }else{
+                    distanza=rigaInizio-rigaFine+1;
+                }
             }
-        }else if(colonnaInizio==colonnaFine){
-            if(rigaInizio<rigaFine){
-                distanza=rigaFine-rigaInizio+1;
-            }else{
-                distanza=rigaInizio-rigaFine+1;
+        }else{
+            if((rigaInizio == rigaFine) && (colonnaInizio == colonnaFine)){
+                return true;
             }
         }
+       
         if(distanza==naveDaAssegnare){
             return true;
         }else{
@@ -125,60 +142,66 @@ public class Griglia {
         }
     }
     private boolean checkAssegnamento(int rigaInizio,int colonnaInizio,int rigaFine,int colonnaFine, int naveDaAssegnare){
-        if(rigaInizio == rigaFine){
-            if(colonnaInizio<colonnaFine){
-                for(int j=colonnaInizio; j<=colonnaFine; j++){
-                    if(griglia[rigaInizio][j].getAssegnato()){
-                        System.out.println("una delle celle da assegnare alla nave è gia assegnata, reinserisci");
-                        return false;
-                    }else{
-                        griglia[rigaInizio][j].setAssegnato();
+        if(naveDaAssegnare!=1){
+            if(rigaInizio == rigaFine){
+                if(colonnaInizio<colonnaFine){
+                    for(int j=colonnaInizio; j<=colonnaFine; j++){
+                        if(griglia[rigaInizio][j].getAssegnato()){
+                            System.out.println("una delle celle da assegnare alla nave è gia assegnata, reinserisci");
+                            return false;
+                        }else{
+                            griglia[rigaInizio][j].setAssegnato();
+                        }
                     }
-                }
-            }else{
-                for(int j=colonnaFine; j<=colonnaInizio; j++){
-                    if(griglia[rigaInizio][j].getAssegnato()){
-                        System.out.println("una delle celle da assegnare alla nave è gia assegnata, reinserisci");
-                        return false;
-                    }else{
-                        griglia[rigaInizio][j].setAssegnato();
+                }else{
+                    for(int j=colonnaFine; j<=colonnaInizio; j++){
+                        if(griglia[rigaInizio][j].getAssegnato()){
+                            System.out.println("una delle celle da assegnare alla nave è gia assegnata, reinserisci");
+                            return false;
+                        }else{
+                            griglia[rigaInizio][j].setAssegnato();
+                        }
                     }
                 }
             }
-        }
-        if(colonnaInizio == colonnaFine){
-            if(rigaInizio<rigaFine){
-                for(int j=rigaInizio; j<=rigaFine; j++){
-                    if(griglia[rigaInizio][j].getAssegnato()){
-                        System.out.println("una delle celle da assegnare alla nave è gia assegnata");
-                        
-                        return false;
-                    }else{
-                        griglia[colonnaInizio][j].setAssegnato();
+            if(colonnaInizio == colonnaFine){
+                if(rigaInizio<rigaFine){
+                    for(int j=rigaInizio; j<=rigaFine; j++){
+                        if(griglia[rigaInizio][j].getAssegnato()){
+                            System.out.println("una delle celle da assegnare alla nave è gia assegnata");
+                            
+                            return false;
+                        }else{
+                            griglia[colonnaInizio][j].setAssegnato();
+                        }
+                    }
+                }else{
+                    for(int j=rigaFine; j<=rigaInizio; j++){
+                        if(griglia[colonnaInizio][j].getAssegnato()){
+                            System.out.println("una delle celle da assegnare alla nave è gia assegnata");
+                            return false;
+                        }else{
+                            griglia[colonnaInizio][j].setAssegnato();
+                        }
                     }
                 }
+            }
+        }else{
+            if(!griglia[rigaInizio][colonnaInizio].getAssegnato()){
+                griglia[rigaInizio][colonnaInizio].setAssegnato();
             }else{
-                for(int j=rigaFine; j<=rigaInizio; j++){
-                    if(griglia[colonnaInizio][j].getAssegnato()){
-                        System.out.println("una delle celle da assegnare alla nave è gia assegnata");
-                        return false;
-                    }else{
-                        griglia[colonnaInizio][j].setAssegnato();
-                    }
-                }
+                return false;
             }
         }
         return true;
     }
-
-
-
+    //fine della zezione per il controllo delle coordinat per quanto riguarda il posizionamento 
     //funzione per controllare il corretto posizionamento della nave 
     private boolean checkCoordinateCorrette(int rigaInizio,int colonnaInizio,int rigaFine,int colonnaFine, int naveDaAssegnare){
         if(!(checkNumeriCorretti(rigaInizio,colonnaInizio) && checkNumeriCorretti(rigaFine,colonnaFine))){
             return false;
         }
-        if(!checkPosizione(rigaInizio,colonnaInizio,rigaFine,colonnaFine)){
+        if(!checkPosizione(rigaInizio,colonnaInizio,rigaFine,colonnaFine,naveDaAssegnare)){
             return false;
         }
         if(!checkDistanza(rigaInizio,colonnaInizio,rigaFine,colonnaFine,naveDaAssegnare)){
@@ -189,14 +212,8 @@ public class Griglia {
         }
         return true;
     }
-    public boolean haPerso(){
-        if(numeroCelleNavi==0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
+    
+    //funzione derivante per il posizionamento della nave che prende le coordinate e le spedisce al controllo con checkcoordinatecorrette
     private void posizionaNave(int naveDaAssegnare){
         int rigaInizio=0;
         int colonnaInizio=0;
@@ -216,6 +233,15 @@ public class Griglia {
         } while (!checkCoordinateCorrette(rigaInizio,colonnaInizio,rigaFine,colonnaFine,naveDaAssegnare));
     }
     
+    //funzione per vedere se il giocatore hga perso, tramite la griglia che conta la qunatita di celle 
+    public boolean haPerso(){
+        if(numeroCelleNavi==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
 
 
     public static void main(String[] args) {
@@ -223,5 +249,6 @@ public class Griglia {
         griglia.inserisciNavi();
         griglia.setCellaColpita(3, 5);
         griglia.printGriglia();
+        
     }
 }

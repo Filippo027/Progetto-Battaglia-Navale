aa2d45905994e6f1849e0d146d9214f43d1509ce
matteoQuerini <matteoqxx@gmail.com>
2025-01-12 19:07:37 +0100

    Test.

--- codice/GameManager.java
+++ codice/GameManager.java
@@ -1,103 +1,49 @@
 public class GameManager {
-    GestoreAccesso gestoreAccesso = new GestoreAccesso("utenti.txt");
+    GestoreAccesso gestoreAccesso = new GestoreAccesso("utenti.txt"); // Oggetto GestoreAccesso
     Utente giocatore1;
     Utente giocatore2;
     int turno = 0;
 
     public GameManager() {
+        // Creiamo i giocatori passandogli il gestore degli accessi
         giocatore1 = new Utente(gestoreAccesso);
         giocatore2 = new Utente(gestoreAccesso);
     }
 
-    public void menu() {
-        boolean continua = true;
-
-        while (continua) {
-            System.out.println("\n==== MENU ====");
-            System.out.println("1. Aggiungi nuovo utente");
-            System.out.println("2. Gioca");
-            System.out.println("3. Visualizza vittorie");
-            System.out.println("4. Esci");
-            System.out.print("Scegli un'opzione: ");
-
-            int scelta = Leggi.unInt();
-
-            switch (scelta) {
-                case 1:
-                    registraNuovoUtente();
-                    break;
-
-                case 2:
-                    gestioneGioco();
-                    break;
-
-                case 3:
-                    visualizzaVittorie();
-                    break;
-
-                case 4:
-                    System.out.println("Arrivederci!");
-                    continua = false;
-                    break;
-
-                default:
-                    System.out.println("Opzione non valida, riprova.");
-            }
-        }
-    }
-
-    public void registraNuovoUtente() {
-
-        System.out.print("Inserisci il nome del giocatore: ");
-        String nome = Leggi.unoString();
-        
-        System.out.print("Inserisci la password: ");
-        String password = Leggi.unoString();
-
-        gestoreAccesso.registraGiocatoreInterattivo(nome, password);
-    }
-
-    public void gestioneGioco(){
+    public void gestioneGioco() {
+        // Introdurre il gioco con i nomi dei giocatori
         System.out.println("Benvenuti al gioco di Battaglia Navale!");
         System.out.println("Giocatore 1: " + giocatore1.getNome());
         System.out.println("Giocatore 2: " + giocatore2.getNome());
         System.out.println("Inizia la partita!\n");
-        
-        turno = 0;
 
-        while(!(giocatore1.haPerso() && giocatore2.haPerso())){
-            if(turno%2==0){
+        // Ciclo del gioco: continua fino a che entrambi i giocatori non perdono
+        while (!(giocatore1.haPerso() && giocatore2.haPerso())) {
+            if (turno % 2 == 0) {
                 System.out.println("=== Turno di " + giocatore1.getNome() + " ===");
                 giocatore1.azioniGiocatore();
-
-            }else{
+            } else {
                 System.out.println("=== Turno di " + giocatore2.getNome() + " ===");
                 giocatore2.azioniGiocatore();
             }
             turno++;
-            System.out.println();
+            System.out.println();  // Righe vuote per separare i turni
         }
-        if(giocatore1.haPerso()){
-            System.out.println("\n==> Ha vinto il " + giocatore2.getNome() + "!");
 
-        }else{
+        // Alla fine del gioco, determiniamo chi ha vinto
+        if (giocatore1.haPerso()) {
+            System.out.println("\n==> Ha vinto il " + giocatore2.getNome() + "!");
+        } else {
             System.out.println("\n==> Ha vinto il " + giocatore1.getNome() + "!");
         }
 
+        // Messaggio finale
         System.out.println("\nLa partita è terminata. Grazie per aver giocato!");
     }
 
-    public void visualizzaVittorie() {
-
-        System.out.print("\nInserisci il nome del giocatore per visualizzare le vittorie: ");
-        String nome = Leggi.unoString();
-
-        int vittorie = gestoreAccesso.getVittorie(nome);
-        System.out.println("Il giocatore " + nome + " ha vinto " + vittorie + " partite.");
-    }
-
     public static void main(String[] args) {
+        // Crea e avvia il gioco
         GameManager gameManager = new GameManager();
-        gameManager.menu();
+        gameManager.gestioneGioco();
     }
 }
